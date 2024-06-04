3f175f23c7489ae12499b1fbbf7cbf548453f37c
srkimdev <emirated3858@gmail.com>
2024-06-04 16:38:35 +0900

    Initial Commit

---
+++ Movie Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Application
+//
+//  Created by 김성률 on 6/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
